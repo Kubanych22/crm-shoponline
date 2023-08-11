41813d112f9a8f9623112580001abf396879dd01
Kubanych22 <z200707@mail.ru>
2023-08-11 14:33:39 +0600

    Module 4, lesson 1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const productName = "Навигационная система Soundmax";
+const productCount = 5;
+const productCategory = 'Техника для дома';
+const productPrice = 100;
+
+const totalCost = productPrice * productCount;
+
+console.log('Наименование товара: \"' + productName + '\"');
+
+console.log('Общая стоимость: $' + totalCost);
