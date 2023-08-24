54a85be61834efbdc1b0710299ccb17a5af7b7d9
Kubanych22 <z200707@mail.ru>
2023-08-24 16:37:58 +0600

    Module 4, lesson08

--- tasks/lesson07/task01.js
+++
@@ -1,9 +0,0 @@
-console.log('%c\nВыполняются скрипты из ДЗ урока 7\nЗадание I\n\nЗадача 1:\n', rainbow);
-
-const genRandomArray1 = (length) => {
- // return Array(length).fill(null).map(() => Math.floor(Math.random() * 100) + 1)
- 
- return Array.from(Array(length)).map(() => Math.floor(Math.random() * 100) + 1)
-}
-
-console.log(genRandomArray1(8))
---
+++ tasks/lesson08/cart.js
@@ -0,0 +1,50 @@
+console.log('%c\nДомашнее задание урока 8:\n', rainbow);8
+
+const cart = {
+  items: [],
+  totalPrice: 0,
+  count: 0,
+  getTotalPrice() {
+    return this.totalPrice;
+  },
+  add(nameProduct, priceProduct, countProduct = 1) {
+    this.increaseCount(countProduct);
+    return this.items.push({nameProduct, priceProduct, countProduct});
+  },
+  increaseCount(countProduct) {
+    this.count += countProduct;
+  },
+  calculateItemPrice() {
+    return this.totalPrice = this.items.reduce((acc, item) => acc + item.countProduct * item.priceProduct, 0);
+  },
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  },
+  print() {
+    console.log('Ваша корзина:');
+    let cloneItems = JSON.stringify(this.items);
+    console.log(cloneItems);
+    console.log(`Общая стоимость ${this.getTotalPrice()} руб.`);
+    
+    // вывод в виде таблицы
+    let summaryTable = JSON.parse(cloneItems);
+    const nameProduct = 'Общая стоимость';
+    const costProduct = this.getTotalPrice();
+    summaryTable.push({nameProduct, costProduct});
+    console.table(summaryTable);
+  }
+};
+
+cart.add('Телефон', 10000, 2);
+cart.add('Компьютер', 70000);
+cart.add('Телевизор', 50000);
+cart.add('Настольная игра', 3500, 3);
+
+cart.calculateItemPrice();
+
+cart.print()
+
+// cart.clear()
+
