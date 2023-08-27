4e412bd00d50ad3d52c42a500fbe21e0374cdf4f
Kubanych22 <z200707@mail.ru>
2023-08-27 11:49:35 +0600

    Module 4, lesson09. Eslint config

--- tasks/lesson09/task2.js
+++ tasks/lesson09/task2.js
@@ -1,5 +1,10 @@
+'use strict';
+
+// eslint-disable-next-line no-undef
 console.log('%c\n\nЗадача 2:\n', rainbow);
 
+// eslint-disable-next-line no-undef
 console.log(`Количество IP-адресов в исходном массиве = ${listIPv4.length}`);
 
+// eslint-disable-next-line max-len,no-undef
 console.log(`Количество уникальных IP-адресов = ${[...new Set(listIPv4)].length}`);
