dd7f27f9e149cc38258ccc23fc60b361af99ea7c
Kubanych22 <z200707@mail.ru>
2023-09-04 11:56:37 +0600

    simplification of the program

--- tasks/final/marbl.js
+++ tasks/final/marbl.js
@@ -9,9 +9,7 @@
   
   const isEven = (number) => number % 2;
   
-  const getChoiceComp = (numberBalls) => {
-    return getRandomIntInclusive(1, numberBalls);
-  };
+  const getChoiceComp = (numberBalls) => getRandomIntInclusive(1, numberBalls);
   
   const checkPlayerInput = (inputValue) => {
     if (!Number.isNaN(parseFloat(inputValue)) && isFinite(inputValue)) {
@@ -25,7 +23,7 @@
     if (inputValue !== null) {
       if (inputValue <= score) {
         const random = getRandomIntInclusive(0, 1);
-        if (isEven(inputValue) === random) {  // Комп угадал
+        if (isEven(inputValue) === random) { // Комп угадал
           result.addCompScore = inputValue;
           result.denyPlayScore = inputValue;
         } else {
@@ -33,12 +31,14 @@
           result.denyCompScore = inputValue;
         }
         return true;
-      } else return false;
+      } else {
+        return false;
+      }
     }
   };
   const checkAnswerPlayer = (answer, computerMoved, result) => {
     if (answer) {
-      if (isEven(computerMoved) === 0) {  // Игрок угадал четное
+      if (isEven(computerMoved) === 0) { // Игрок угадал четное
         result.addPlayScore = computerMoved;
         result.denyCompScore = computerMoved;
       } else {
@@ -46,7 +46,7 @@
         result.denyPlayScore = computerMoved;
       }
     } else {
-      if (isEven(computerMoved) === 1) {  // Игрок угадал нечетное
+      if (isEven(computerMoved) === 1) { // Игрок угадал нечетное
         result.addPlayScore = computerMoved;
         result.denyCompScore = computerMoved;
       } else {
@@ -54,8 +54,128 @@
         result.denyPlayScore = computerMoved;
       }
     }
-  }
+  };
   
+  const movePlayer = (playerScore, computerScore, result, flag) => {
+    let numberBalls;
+    let error;
+    let str;
+    if (lang === 0) {
+      str = guessNumberRu(playerScore, computerScore);
+      numberBalls = prompt(str);
+      error = computerGuesses(numberBalls, playerScore, result);
+    } else {
+      str = guessNumberEn(playerScore, computerScore);
+      numberBalls = prompt(str);
+      error = computerGuesses(numberBalls, playerScore, result);
+    }
+    return flag = !error;
+  };
+  
+  const guessNumberRu = (playerScore, computerScore) => {
+    return `Количество шариков у вас: ` + `${playerScore}` + `\n` +
+      `Количество шариков у компьютера: ` + `${computerScore}` + `\n` +
+      `Ваш ход. Загадайте число от 1 до ` + `${playerScore}`;
+  };
+  const guessNumberEn = (playerScore, computerScore) => {
+    return `You have: ` + `${playerScore}` + ` balls\n` +
+      `The number of balls at the computer: ` + `${computerScore}` + `\n` +
+      `Guess the number from 1 to ` + `${playerScore}`;
+  };
+  const answerRu = (result) => {
+    return `Количество шариков у вас: ${result.playerScore}\n` +
+      `Количество шариков у компьютера: ${result.compScore}\n` +
+      `Компьютер загадал число.\n` +
+      `Число четное?`;
+  };
+  const answerEn = (result) => {
+    return `The number of balls you have: ${result.playerScore}\n` +
+      `The number of balls at the computer: ${result.compScore}\n` +
+      `The computer made a number.\n` +
+      `Is the number even?`;
+  };
+  const moveComputer = (computerScore, result) => {
+    let computerMoved;
+    let answer;
+    let str;
+    if (lang === 0) {
+      str = answerRu(result);
+      answer = confirm(str);
+      computerMoved = getRandomIntInclusive(1, computerScore);
+      checkAnswerPlayer(answer, computerMoved, result);
+    } else {
+      str = answerEn(result);
+      answer = confirm(str);
+      computerMoved = getRandomIntInclusive(1, computerScore);
+      checkAnswerPlayer(answer, computerMoved, result);
+    }
+  };
+  const playerWonRu = (result) => {
+    return `Вы выиграли!\nКоличество шариков у вас: ${result.playerScore}\n` +
+      `Количество шариков у компьютера: ${result.compScore}`;
+  }
+  const playerWonEn = (result) => {
+    return `You have won!\nThe number of balls you have: ${result.playerScore}\n` +
+      `The number of balls at the computer: ${result.compScore}`;
+  }
+  const playerWon = (result) => {
+    let str;
+    if (lang === 0) {
+      str = playerWonRu(result)
+      alert(str);
+      if (confirm('Хотите сыграть еще?')) {
+        result.restore();
+        window.repeatGame = true;
+        return true;
+      } else {
+        window.repeatGame = false;
+        return false;
+      }
+    }
+    str = playerWonEn(result)
+    alert(str);
+    if (confirm('Do you want to play some more?')) {
+      result.restore();
+      window.repeatGame = true;
+      return true;
+    } else {
+      window.repeatGame = false;
+      return false;
+    }
+  };
+  const computerWonRu = (result) => {
+    return `Выиграл компьютер!\nКоличество шариков у вас: ${result.playerScore}\n` +
+    `Количество шариков у компьютера: ${result.compScore}`
+  }
+  const computerWonEn = (result) => {
+    return `Won the computer!\nThe number of balls you have: ${result.playerScore}\n` +
+      `The number of balls at the computer: ${result.compScore}`
+  }
+  const computerWon = (result) => {
+    let str;
+    if (lang === 0) {
+      str = computerWonRu(result)
+      alert(str);
+      if (confirm('Хотите сыграть еще?')) {
+        result.restore();
+        window.repeatGame = true;
+        return true;
+      } else {
+        window.repeatGame = false;
+        return false;
+      }
+    }
+    str = computerWonEn(result)
+    alert(str);
+    if (confirm('Do you want to play some more?')) {
+      result.restore();
+      window.repeatGame = true;
+      return true;
+    } else {
+      window.repeatGame = false;
+      return false;
+    }
+  }
   const marble = (whoWon, lang) => {
     const result = {
       player: 5,
@@ -96,17 +216,17 @@
           this.computer = 0;
         }
       },
-  
-      restore () {
+      
+      restore() {
         this.computer = 5;
         this.player = 5;
-      }
+      },
     };
+    
     let flag;
     return function start(whoWon, lang) {
       let playerScore = result.playerScore;
-      let compScore = result.compScore;
-      let numberBalls;
+      let computerScore = result.compScore;
       if (whoWon === 0) {
         flag = true;
         whoWon = -1;
@@ -114,87 +234,20 @@
         flag = false;
         whoWon = -1;
       }
-      if (flag) {  // Ход игрока
-        let error;
-        if (lang === 0) {
-          numberBalls = prompt(`Количество шариков у вас: ${playerScore}\n` +
-            `Количество шариков у компьютера: ${compScore}\n` +
-            `Ваш ход. Загадайте число от 1 до ${playerScore}`);
-          error = computerGuesses(numberBalls, playerScore, result);
-          flag = !error;
-        } else {
-          numberBalls = prompt(`You have ${playerScore} balls.\n` +
-          `The number of balls at the computer: ${compScore}\n` +
-          `Guess the number from 1 to ${playerScore}`);
-          error = computerGuesses(numberBalls, playerScore, result);
-          flag = !error;
-        }
-      } else {  // Ход компьютера
+      if (flag) { // Ход игрока
+        flag = movePlayer(playerScore, computerScore, result, flag);
+      } else { // Ход компьютера
         flag = true;
-        let computerMoved;
-        let answer;
-        if (lang === 0) {
-          answer = confirm(`Количество шариков у вас: ${result.playerScore}\n` +
-            `Количество шариков у компьютера: ${result.compScore}\n` +
-            `Компьютер загадал число.\n` +
-            `Число четное?`);
-          computerMoved = getRandomIntInclusive(1, compScore);
-          checkAnswerPlayer(answer, computerMoved, result);
-        } else {
-          answer = confirm(`The number of balls you have: ${result.playerScore}\n` +
-            `The number of balls at the computer: ${result.compScore}\n` +
-            `The computer made a number.\n` +
-            `Is the number even?`);
-          computerMoved = getRandomIntInclusive(1, compScore);
-          checkAnswerPlayer(answer, computerMoved, result);
-        }
+        moveComputer(computerScore, result);
       }
       // определение победителя
-      if (result.compScore === 0) {
-        if (lang === 0) {
-          alert(`Вы выиграли!\n` + `Количество шариков у вас: ${result.playerScore}\n` +
-            `Количество шариков у компьютера: ${result.compScore}`);
-          if (confirm('Хотите сыграть еще?')) {
-            result.restore();
-            window.repeatGame = true;
-            return;
-          } else {
-            window.repeatGame = false;
-            return;
-          }
-        }
-        alert(`You have won!\n` + `The number of balls you have: ${result.playerScore}\n` +
-          `The number of balls at the computer: ${result.compScore}`);
-        if (confirm('Do you want to play some more?')) {
-          result.restore();
-          window.repeatGame = true;
-          return;
-        } else {
-          window.repeatGame = false;
-          return;
-        }
+      if (result.compScore === 0) {        
+        playerWon(result);
+        return;
       }
       if (result.playerScore === 0) {
-        if (lang === 0) {
-          alert(`Выиграл компьютер!\n` + `Количество шариков у вас: ${result.playerScore}\n` +
-            `Количество шариков у компьютера: ${result.compScore}`);
-          if (confirm('Хотите сыграть еще?')) {
-            result.restore();
-            window.repeatGame = true;
-            return;
-          } else {
-            window.repeatGame = false;
-            return;
-          }
-        }
-        alert(`Won the computer!\n` + `The number of balls you have: ${result.playerScore}\n` +
-          `The number of balls at the computer: ${result.compScore}`);
-        if (confirm('Do you want to play some more?')) {
-          result.restore();
-          return;
-        } else {
-          return;
-        }
+        computerWon(result); 
+        return;
       }
       return start(whoWon, lang);
     };
