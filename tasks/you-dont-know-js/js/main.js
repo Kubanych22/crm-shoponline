2bfcd61705b7825ca3fec5c5225bf15c2a4c9684
Kubanych22 <z200707@mail.ru>
2023-09-09 22:06:32 +0600

    modified main.js

--- tasks/you-dont-know-js/js/main.js
+++ tasks/you-dont-know-js/js/main.js
@@ -6,7 +6,7 @@ const swapNode = (nodeA, nodeB) => {
   
   nodeB.parentNode.append(nodeA, nodeB);
   parentA.append(nodeB, siblingA);
-}
+};
 
 const items = document.querySelectorAll('.item');
 const itemTwo = document.querySelector('.item_two');
@@ -18,7 +18,7 @@ items[4].before(itemFour);
 
 itemFour = document.querySelectorAll('.props__item_four');
 console.log(itemFour);
-itemFour[3].after(itemTwoPropsFour)
+itemFour[3].after(itemTwoPropsFour);
 const itemFive = document.querySelector('.item_five');
 const itemFiveTitle = itemFive.querySelector('.item__title');
 
@@ -27,16 +27,16 @@ const sixPropsListItems = itemSix.querySelectorAll('.item_six .props__item');
 
 const propsList = document.querySelectorAll('.props__list');
 let item = sixPropsListItems[8].cloneNode(true);
-propsList[1].append(item)
+propsList[1].append(item);
 item = sixPropsListItems[9].cloneNode(true);
-propsList[1].append(item)
+propsList[1].append(item);
 
 const itemThree = document.querySelector('.item_three');
 const itemThreeTitle = itemThree.querySelector('.item__title');
 itemThreeTitle.innerHTML = 'This и прототипы объектов';
-const itemThreePropsList = itemThree.querySelector('.props__list')
+const itemThreePropsList = itemThree.querySelector('.props__list');
 
-const itemFivePropsList = itemFive.querySelector('.props__list')
+const itemFivePropsList = itemFive.querySelector('.props__list');
 
 swapNode(itemThreePropsList, itemFivePropsList);
 
@@ -45,18 +45,13 @@ let lastChild = itemSix.querySelector('li:last-child');
 lastChild.remove();
 lastChild = itemSix.querySelector('li:last-child');
 lastChild.remove();
-itemTwoTitle.before(itemFiveTitle)
+itemTwoTitle.before(itemFiveTitle);
 const itemFiveTitleNew = document.createElement('h2');
 itemFiveTitleNew.classList.add('.item__title');
-itemSixTitle.before(itemTwoTitle)
+itemSixTitle.before(itemTwoTitle);
 const itemSixTitleList = itemSix.querySelectorAll('h2');
-itemSixTitleList[1].remove()
-itemFiveTitleNew.textContent = itemSixTitleList[1].textContent
-const itemFiveNew = itemFive.querySelector('.content')
-const itemFiveNewImageNode = itemFiveNew.querySelector('.item__image')
-itemFiveNewImageNode.after(itemFiveTitleNew)
-
-
-
-
-
+itemSixTitleList[1].remove();
+itemFiveTitleNew.textContent = itemSixTitleList[1].textContent;
+const itemFiveNew = itemFive.querySelector('.content');
+const itemFiveNewImageNode = itemFiveNew.querySelector('.item__image');
+itemFiveNewImageNode.after(itemFiveTitleNew);
